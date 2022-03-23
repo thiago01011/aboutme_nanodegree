fac774d33fb5000f6fac9fefc2ada7da53a89dfd
Thiago Paulino Silva Galindo <thiago.galindo@venturus.org.br>
2022-03-23 17:18:36 -0300

    About me app with Data Binding.

--- app/src/main/java/com/galindo/aboutmenanodegree/MainActivity.kt
+++ app/src/main/java/com/galindo/aboutmenanodegree/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.galindo.aboutmenanodegree
 
+import android.content.Context
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import com.galindo.aboutmenanodegree.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.doneButton.setOnClickListener {
+            addNickname(it)
+        }
     }
+
+    private fun addNickname(view: View) {
+        binding.apply {
+            nicknameText.text = binding.nicknameEdit.text
+            // Refresh UI
+            invalidateAll()
+            nicknameText.visibility = View.VISIBLE
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+        }
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+
+    }
+
+
 }
